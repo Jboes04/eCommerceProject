9d0da5a7fffaac79d850ca83a64b5c28794e8266
Boes Julian <julian.boes@decathlon.com>
2018-06-12 15:40:57 +0200

    christophe utility

---
+++ src/modules/product/productUtility.js
@@ -0,0 +1,6 @@
+export function completeDisplay(products) {
+  return products.map((product) => {
+    product.image_path = "https://www.decathlon.fr/media/" + product.image_path;
+    return product;
+  });
+}
