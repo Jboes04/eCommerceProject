2abe241cf4982ead31711eb719ac0387967c4662
Boes Julian <julian.boes@decathlon.com>
2018-06-14 09:28:27 +0200

    categorie julian

--- src/modules/basket/Navbar.js
+++ src/modules/basket/Navbar.js
@@ -6,6 +6,11 @@ class Navbar extends Component {
   render() {
     return (
       <div className="pos-f-t">
+        <nav className="navbar navbar-dark bg-primary">
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <i className="fas fa-cart-arrow-down"></i>
+          </button>
+        </nav>
         <div className="collapse" id="navbarToggleExternalContent">
           <div className="bg-light p-4">
             <h5 className="text-dark h4">RECAPITULATIF DE VOTRE PANIER</h5>
@@ -13,11 +18,6 @@ class Navbar extends Component {
             <Basket />
           </div>
         </div>
-        <nav className="navbar navbar-dark bg-primary">
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-            <i className="fas fa-cart-arrow-down"></i>
-          </button>
-        </nav>
       </div>
     );
   }
