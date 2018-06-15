18b24681b5ace77f7c9de4477896f35adfcda364
Martin D <martin.delorme@decathlon.com>
2018-06-15 12:13:46 +0200

    agree checkbox

--- src/modules/checkout/Delivery.js
+++ src/modules/checkout/Delivery.js
@@ -105,9 +105,8 @@ handleChangePostCode = (event) => {
         </div>
         <div className="form-group">
           <div className="form-check">
-
+            <input class="form-check-input is-invalid" type="checkbox" value="" id="invalidCheck3" required />
             <label className="form-check-label" htmlFor="invalidCheck3">
-
               Agree to terms and conditions
             </label>
             <div className="invalid-feedback">
