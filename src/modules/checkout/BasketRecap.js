0066101e62492a093de83add443db54da8ba819a
Martin D <martin.delorme@decathlon.com>
2018-06-15 08:44:17 +0200

    basket recap

--- src/modules/checkout/BasketRecap.js
+++ src/modules/checkout/BasketRecap.js
@@ -5,6 +5,7 @@ import { getBasketList } from "./../../store/basket/selectors";
 import {formatAmount} from "./../../util.js";
 import {getTotalAmount, getPaymentAmount} from './../basket/basketUtility';
 import StripeCheck from './../stripe/StripeCheckout';
+import './../../App.css';
 
 class BasketRecap extends Component {
 
@@ -21,6 +22,7 @@ getLocalInfo = () => {
           <div className="col-4">
 
         <ul className="list-group">
+          <li className="BasketRecap list-group-item">Adresse de livraison</li>
           <li className="list-group-item">{this.getLocalInfo().firstName} {this.getLocalInfo().lastName}</li>
           <li className="list-group-item">{this.getLocalInfo().address}</li>
           <li className="list-group-item">{this.getLocalInfo().postcode} - {this.getLocalInfo().city}</li>
@@ -28,10 +30,27 @@ getLocalInfo = () => {
         </div>
         <div className="col-8">
           <ul className="list-group">
+            <li className="BasketRecap list-group-item">Liste des articles de votre panier</li>
+            <li className="BasketRecap list-group-item">
+              <th width="350">Article</th>
+              <th width="110">Quantité</th>
+              <th width="120">Prix unitaire</th>
+              <th width="120">Prix total</th>
+            </li>
             {this.props.basketList.map((product) =>
-              <li key={product.productId} className="list-group-item">{product.label} - {product.quantity} - {formatAmount(product.price)} - {formatAmount(product.quantity*product.price)}</li>
+              <li key={product.productId} className="list-group-item">
+                <td width="350">{product.label}</td>
+                <td width="110">{product.quantity}</td>
+                <td width="120">{formatAmount(product.price)}</td>
+                <td width="120">{formatAmount(product.quantity*product.price)}</td>
+              </li>
             )}
-            <li className="list-group-item">TOTAL PRICE: {formatAmount(getTotalAmount(this.props.basketList))}</li>
+            <li className="BasketRecap list-group-item">
+              <td width="350"></td>
+              <td width="110"></td>
+              <td width="120">PRIX TOTAL</td>
+              <td width="120">{formatAmount(getTotalAmount(this.props.basketList))}</td>
+            </li>
 
           </ul>
         </div>
